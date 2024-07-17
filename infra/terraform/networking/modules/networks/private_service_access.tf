d53f9e5a3f317fe4342a7457f4f994de0a45ff6b
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2024-07-17 19:23:49 -0400

    Add backend.tf to networking module

--- infra/terraform/networking/modules/networks/private_service_access.tf
+++ infra/terraform/networking/modules/networks/private_service_access.tf
@@ -6,7 +6,7 @@ resource "google_compute_global_address" "private_service_access_address" {
   network       = google_compute_network.shared.id
 }
 
-resource "google_service_networking_connection" "private_vpc_connection" {
+resource "google_service_networking_connection" "private_service_access" {
   network = google_compute_network.shared.id
   service = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [
